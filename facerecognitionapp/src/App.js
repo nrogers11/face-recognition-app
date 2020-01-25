dc647a5a9cc55cceebaa983a616785cf1bc9bd4e
Benicio Daniel Barreto <beni@MacBook-Pro-de-Benicio.local>
2020-01-25 00:28:59 -0300

    Add the particles below

--- facerecognitionapp/src/App.js
+++ facerecognitionapp/src/App.js
@@ -2,13 +2,29 @@ import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
 import './App.css';
+
+const particleOptions = {
+  particles: {
+    number: {
+      value: 50,
+      density: {
+        enable: true,
+        value_area: 400
+      }
+    },
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles' params={ particleOptions } />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>
