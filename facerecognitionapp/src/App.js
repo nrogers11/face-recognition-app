6c02e751095ebf6702cb5eb8da980c8b803f1dd1
Benicio Daniel Barreto <beni@MacBook-Pro-de-Benicio.local>
2020-01-25 22:58:24 -0300

    Making the Sign in form with a event handler on submit button

--- facerecognitionapp/src/App.js
+++ facerecognitionapp/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
+import SignIn from './components/SignIn/SignIn';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -30,7 +31,8 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl: '',
-      box: {}
+      box: {},
+      route: 'signin',
     }
   }
 
@@ -68,21 +70,30 @@ class App extends Component {
     .catch(error => console.log(error));
   }
 
+  onRouteChange = () => {
+    this.setState({route: 'home'});
+  }
+
   render() {
     return (
       <div className="App">
         <Particles className='particles' params={ particleOptions } />
         <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm 
-          onInputChange={this.onInputChange} 
-          onButtonSubmit={this.onButtonSubmit} 
-        />
-        <FaceRecognition 
-          box={this.state.box}
-          imageUrl={this.state.imageUrl} 
-        />
+        { this.state.route === 'signin' 
+            ? <SignIn onRouteChange={this.onRouteChange} />
+            : <div>
+              <Logo />
+              <Rank />
+              <ImageLinkForm 
+                onInputChange={this.onInputChange} 
+                onButtonSubmit={this.onButtonSubmit} 
+              />
+              <FaceRecognition 
+                box={this.state.box}
+                imageUrl={this.state.imageUrl} 
+              />
+            </div> 
+        }
       </div>
     );
   }
