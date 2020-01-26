a02da38d9e9cbb4ff186002ac87e76a11a958f04
Benicio Daniel Barreto <beni@MacBook-Pro-de-Benicio.local>
2020-01-26 10:43:40 -0300

    Register and Navigation

--- facerecognitionapp/src/components/Navigation/Navigation.js
+++ facerecognitionapp/src/components/Navigation/Navigation.js
@@ -1,15 +1,25 @@
 import React from 'react';
 
-const Navigation = ({onRouteChange}) => {
+const Navigation = ({onRouteChange, isSignedIn}) => {
   return (
-    <nav style={{ display: 'flex', justifyContent: 'flex-end'}}>
-      <p 
-        onClick={ () => onRouteChange('signin') } 
-        className='f3 link dim black underline pa3 pointer'
-      >
-          Sign Out
-      </p>
-    </nav>
+    if (isSignedIn) {
+      <nav style={{ display: 'flex', justifyContent: 'flex-end'}}>
+        <p onClick={ () => onRouteChange('signin') } className='f3 link dim black underline pa3 pointer'>
+            Sign Out
+        </p>
+      </nav> 
+    } else {
+      <nav style={{ display: 'flex', justifyContent: 'flex-end'}}>
+        <p onClick={ () => onRouteChange('signin') } className='f3 link dim black underline pa3 pointer'>
+            Sign In
+        </p>
+      </nav>
+      <nav style={{ display: 'flex', justifyContent: 'flex-end'}}>
+        <p onClick={ () => onRouteChange('signin') } className='f3 link dim black underline pa3 pointer'>
+            Register
+        </p>
+      </nav>
+    }
   );
 }
 
