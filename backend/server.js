6612e278efab3c10f3fac037b45cc5e8ac9e4e43
Benicio Daniel Barreto <beni@MacBook-Pro-de-Benicio.local>
2020-01-30 19:57:25 -0300

    Implementing the Signin sketch and testing in Insomnia

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,39 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+const dataBase = {
+  users: [
+    {
+      id: '123',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'hamburguer',
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: '321',
+      name: 'Joca',
+      email: 'joca@gmail.com',
+      password: 'batata',
+      entries: 0,
+      joined: new Date(),
+    }
+  ]
+}
+
 app.get('/', (req, res) => {
   res.send("hi, there!");
+})
+
+app.post('/signin', (req, res) => {
+  if (req.body.email === dataBase.users[0].email && req.body.password === dataBase.users[0].password) {
+    res.json("entrou");
+  } else {
+    res.status(400).json("error logging in");
+  }
 })
 
 app.listen(3000, () => {
@@ -11,7 +42,6 @@ app.listen(3000, () => {
 })
 
 /*
-
 /                 --> res = this is working
 /signiin          --> POST = success / fail
 /register         --> POST = user (object)
