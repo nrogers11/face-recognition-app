76f0fe20948eedbe7f16f671f148186f25e266ee
Benicio Daniel Barreto <beni@MacBook-Pro-de-Benicio.local>
2020-02-08 17:07:15 -0300

    Refactoring route image

--- backend/server.js
+++ backend/server.js
@@ -98,17 +98,13 @@ app.get('/profile/:id', (req, res) => {
 
 app.put('/image', (req, res) => {
   const { id } = req.body;
-  let found = false;
-  dataBase.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }  
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    res.json(entries[0]);
   })
-  if(!found){
-    res.status(400).json("not found");
-  }
+  .catch(error => res.status(400).json('Unable to get entries!'));
 })
 
 app.listen(3001, () => {
