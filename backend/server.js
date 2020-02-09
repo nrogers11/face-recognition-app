fa7c3bb42dc0dbb45a9f368757af3db665c55649
Benicio Daniel Barreto <beni@MacBook-Pro-de-Benicio.local>
2020-02-09 12:24:06 -0300

    Implements SignIn flow

--- backend/server.js
+++ backend/server.js
@@ -55,17 +55,22 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
-  bcrypt.compare("songoku", '$2a$10$RYtAPMkDbJmIiCX2Wew37.nz1.L.Zukd/Nsu1xduMFYpXA4D1LWa.', function(err, res) {
-    console.log("first guess", res)
-  });
-  bcrypt.compare("veggies", '$2a$10$RYtAPMkDbJmIiCX2Wew37.nz1.L.Zukd/Nsu1xduMFYpXA4D1LWa.', function(err, res) {
-    console.log("second guess", res)
-  });
-  if (req.body.email === dataBase.users[0].email && req.body.password === dataBase.users[0].password) {
-    res.json(dataBase.users[0]);
-  } else {
-    res.status(400).json("error logging in");
-  }
+  db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return db.select('*').from('users')
+          .where('email', '=', req.body.email)
+          .then(user => {
+            res.json(user[0]);
+          })
+          .catch(error => res.status(400).json('Unable to get user'));
+      } else {
+        res.status(400).json('Wrong credentials!');
+      }
+    })
+    .catch(error => res.status(400).json('Wrong credentials!'))
 })
 
 app.post('/register', (req, res) => {
