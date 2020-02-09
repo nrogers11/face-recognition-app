9ee6fe9ef7323719337de44d99f77ea5a8b9e668
Benicio Daniel Barreto <beni@MacBook-Pro-de-Benicio.local>
2020-02-09 12:01:14 -0300

    Implements transaction in register

--- backend/server.js
+++ backend/server.js
@@ -70,15 +70,28 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
   const { email, password, name} = req.body;
-  db('users')
-    .returning('*')
-    .insert({
+  const hash = bcrypt.hashSync(password);
+  db.transaction(trx => {
+    trx.insert({
+      hash: hash,
       email: email,
-      name: name,
-      joined: new Date()
-  })
-    .then(user => {
-      res.json(user[0]);
+    })
+    .into('login')
+    .returning('email')
+    .then( loginEmail => {
+      return trx('users')
+        .returning('*')
+        .insert({
+          email: loginEmail[0],
+          name: name,
+          joined: new Date()
+        })
+        .then(user => {
+          res.json(user[0]);
+        })
+    })
+    .then(trx.commit)
+    .catch(trx.rollback)
   })
     .catch(error => res.status(400).json('unable to register'));
 })
